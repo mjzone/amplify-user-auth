a82385b3daad50ca7bd6bfc3c44978b628753656
Manoj Fernando <manojf@99x.lk>
2020-12-21 12:14:00 +0530

    initial commit

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateBook = /* GraphQL */ `
+  subscription OnCreateBook {
+    onCreateBook {
+      id
+      title
+      description
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateBook = /* GraphQL */ `
+  subscription OnUpdateBook {
+    onUpdateBook {
+      id
+      title
+      description
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteBook = /* GraphQL */ `
+  subscription OnDeleteBook {
+    onDeleteBook {
+      id
+      title
+      description
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
